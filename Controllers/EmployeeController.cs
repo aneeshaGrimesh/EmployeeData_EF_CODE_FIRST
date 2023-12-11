47ca0cc4742e0d244a1196d5376a17801dbce446
Aneesha <aneesha.nu123@gmail.com>
2023-12-11 04:32:09 -0800

    Create added

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeData.DAL;
 using EmployeeData.Models;
+using EmployeeData.Models.DBEntity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeData.Controllers
@@ -16,28 +17,79 @@ namespace EmployeeData.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            var employee = _context.Employees.ToList();
-            List<EmployeeViewModel> employeelIst = new List<EmployeeViewModel>();
-            if (employee != null )
+            try
             {
-              
-                foreach(var emp in employee)
+                var employee = _context.Employees.ToList();
+                List<EmployeeViewModel> employeelIst = new List<EmployeeViewModel>();
+                if (employee != null)
                 {
-                    var EmployeeViewModel = new EmployeeViewModel()
+
+                    foreach (var emp in employee)
                     {
-                        Id = emp.Id,
-                        FirstName = emp.FirstName,
-                        LastName = emp.LastName,
-                        DateofBirth = emp.DateofBirth,
-                        Salary = emp.Salary,
-                        Email = emp.Email,
-                        
-                    };
-                    employeelIst.Add(EmployeeViewModel);
+                        var EmployeeViewModel = new EmployeeViewModel()
+                        {
+                            Id = emp.Id,
+                            FirstName = emp.FirstName,
+                            LastName = emp.LastName,
+                            DateofBirth = emp.DateofBirth,
+                            Salary = emp.Salary,
+                            Email = emp.Email,
+
+                        };
+                        employeelIst.Add(EmployeeViewModel);
+                    }
+                    return View(employeelIst);
                 }
                 return View(employeelIst);
             }
-            return View(employeelIst);
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+        }// return get employee page view with employee view model
+
+
+        [HttpGet]
+        public IActionResult Create() // return Create page view with employee view model
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeeViewModel employeedata)
+        {
+            try
+            {
+                if (ModelState.IsValid)// validate model values from ui 
+                {
+                    var employee = new Employee() //this object will communicate to DB
+                    {
+                        FirstName = employeedata.FirstName,
+                        LastName = employeedata.LastName,
+                        DateofBirth = employeedata.DateofBirth,
+                        Email = employeedata.Email,
+                        Salary = employeedata.Salary
+                    };
+                    _context.Employees.Add(employee);
+                    _context.SaveChanges();//built in function
+                    TempData["SuccessMessage"] = "Employee created successfully";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = "Model data not valid";
+                    return View();// same page
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+           
+           
         }
     }
 }
