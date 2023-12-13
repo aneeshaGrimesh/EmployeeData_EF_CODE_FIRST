3daa7d6429e6d9371b38aa8f48589f09a0d49687
Aneesha <aneesha.nu123@gmail.com>
2023-12-13 08:21:30 -0800

    Upadte and delete added

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -2,6 +2,7 @@
 using EmployeeData.Models;
 using EmployeeData.Models.DBEntity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace EmployeeData.Controllers
 {
@@ -90,6 +91,146 @@ namespace EmployeeData.Controllers
             }
            
            
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            try
+            {
+                //SingleOrDefault return Single row of data
+                var employee = _context.Employees.SingleOrDefault(x => x.Id == Id);
+                if (employee != null)
+                {
+                    var employeeview = new EmployeeViewModel()
+                    {
+                        FirstName = employee.FirstName,
+                        LastName = employee.LastName,
+                        Email = employee.Email,
+                        Salary = employee.Salary,
+                        DateofBirth = employee.DateofBirth,
+                        Id = employee.Id
+                    };
+                    //passing emlopyeemodel to UI
+                    return View(employeeview);
+                }
+                else
+                {
+                    TempData["errorMessage"] = $"Employee Details not avilable with this Id:{Id}";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+            
+           
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel emp)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var employee = new Employee()
+                    {
+                        FirstName = emp.FirstName,
+                        Id = emp.Id,
+                        LastName = emp.LastName,
+                        Email = emp.Email,
+                        Salary = emp.Salary,
+                        DateofBirth = emp.DateofBirth
+
+
+                    };
+                    _context.Employees.Update(employee);
+                    _context.SaveChanges();
+                    TempData["SuccessMessage"] = "Employee details updated succesfully!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = "Model data is invalid!";
+                    return View(); /*View itself same page*/
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+           
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            try
+            {
+                //SingleOrDefault return Single row of data
+                var employee = _context.Employees.SingleOrDefault(x => x.Id == Id);
+                if (employee != null)
+                {
+                    var employeeview = new EmployeeViewModel()
+                    {
+                        FirstName = employee.FirstName,
+                        LastName = employee.LastName,
+                        Email = employee.Email,
+                        Salary = employee.Salary,
+                        DateofBirth = employee.DateofBirth,
+                        Id = employee.Id
+                    };
+                    //passing emlopyeemodel to UI
+                    return View(employeeview);
+                }
+                else
+                {
+                    TempData["errorMessage"] = $"Employee Details not avilable with this Id:{Id}";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete (EmployeeViewModel emp) {
+
+            try
+            {
+                var employee = _context.Employees.SingleOrDefault(x => x.Id == emp.Id);
+                if (employee != null)
+                {
+                    _context.Employees.Remove(employee);
+                    _context.SaveChanges();
+                    TempData["SuccessMessage"] = "Employee Deleted successfully";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = $"Employee Details not avilable with this Id:{emp.Id}";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+                
         }
     }
 }
